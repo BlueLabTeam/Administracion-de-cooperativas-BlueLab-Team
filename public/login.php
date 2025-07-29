765365025472cf13f6cacd70d3761b587c704638
César Pérez <cesargabriel2007perez@gmail.com>
2025-07-29 00:57:39 -0300

    se realiza el frontend de la page login

--- public/login.php
+++ public/login.php
@@ -1,11 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
-</head>
-<body>
-    
-</body>
+    <link rel="stylesheet" href="assets/css/styles.css" />
+  </head>
+
+  <body>
+    <?php include 'includes/header.php'; ?>
+    <main class="login__main">
+      <div class="login__container">
+        <div class="login__subcontainer">
+          <h1 class="login__titulo">Cooperativa de Viviendas</h1>
+          <p class="login__subtitulo">Sistema Administrativo</p>
+          <form
+            action="/app/controllers/usuarioController.php"
+            class="login__formulario"
+          >
+            <label for="usuario" class="formulario__label">Usuario</label>
+            <input
+              type="text"
+              class="login__input input--usuario"
+              name="usuario"
+              placeholder="Ingrese su usuario"
+            />
+            <label for="contrasenia" class="formulario__label"
+              >Contraseña</label
+            >
+            <input
+              type="password"
+              class="login__input input--contrasenia"
+              name="contrasenia"
+              placeholder="Ingrese su contraseña"
+            />
+            <input type="submit" class="login__submit" value="Ingresar" />
+          </form>
+        </div>
+      </div>
+    </main>
+    <?php include 'includes/footer.php'; ?>
+  </body>
 </html>
