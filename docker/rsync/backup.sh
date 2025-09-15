5269b4a815ab0736d1038578319347510dbd2867
César Pérez <cesargabriel2007perez@gmail.com>
2025-09-15 04:48:48 -0300

    Se agrega el script de backup y se actualiza el docker compose para su funcionamiento v1

--- docker/rsync/backup.sh
+++ docker/rsync/backup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# Carpeta origen
-SRC=/data
-# Carpeta destino (puede ser otro volumen montado)
-DEST=/backup/gestcoop-$(date +%F)
-mkdir -p $DEST
-rsync -av --delete $SRC/ $DEST/
+logger "Backup iniciado"
 
-mysqldump -u root -p$MYSQL_ROOT_PASSWORD $DB_NAME > /backup/db-$(date +%F).sql
+SRC=/data/uploads
+DEST=/backups/gestcoop-$(date +%F)
+mkdir -p $DEST
+
+rsync -av --delete $SRC/ $DEST/ | logger -t rsync
+
+
+mysqldump -u root -p$MYSQL_ROOT_PASSWORD $DB_NAME > /backups/db-$(date +%F).sql
+logger "mysqldump completado"
+
+logger "Backup completado"
