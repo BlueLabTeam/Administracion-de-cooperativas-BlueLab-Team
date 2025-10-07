2ca9032ed8e102200f66e9a1131b2e2c87af480d
cesar <cesargabriel2007perez@gmail.com>
2025-10-06 23:59:27 -0300

    Fix: Se eliminan archivos innecesarios

--- docker/db/master/init-master.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS testdb;
-
-CREATE USER 'replicador'@'%' IDENTIFIED WITH mysql_native_password BY 'replicapass';
-GRANT REPLICATION SLAVE ON *.* TO 'replicador'@'%';
-FLUSH PRIVILEGES;
