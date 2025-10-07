677722c5641756be30aae466b68c8e67a6fcbb65
cesar <cesargabriel2007perez@gmail.com>
2025-10-07 01:07:05 -0300

    Feature: Se realizan las modificaciones necesarias para implementar variables de entorno y no mantener credenciales y datos hardcodeados

---
+++ docker/db/master/init-master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "⏳ Creando usuario replicador..."
+
+mysql -uroot -p"$DB_ROOT_PASSWORD" -e "
+CREATE USER IF NOT EXISTS '${DB_REPLICATOR_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${DB_REPLICATOR_PASSWORD}';
+GRANT REPLICATION SLAVE ON *.* TO '${DB_REPLICATOR_USER}'@'%';
+FLUSH PRIVILEGES;
+"
+
+echo "✅ Master inicializado correctamente."
--- docker/db/slave/init-slave.sh
+++ docker/db/slave/init-slave.sh
@@ -2,22 +2,22 @@
 set -e
 
 echo "⏳ Esperando que el Master esté disponible..."
-until mysql -h gestcoop_db -uroot -p${DB_ROOT_PASSWORD} -e "SELECT 1" &> /dev/null ; do
+until mysql -h "${DB_HOST}" -uroot -p"${DB_ROOT_PASSWORD}" -e "SELECT 1" &> /dev/null ; do
 sleep 2
 done
 echo "✅ Master disponible, configurando Slave..."
 
 # Obtener archivo y posición del Master
-MASTER_STATUS=$(mysql -h gestcoop_db -uroot -p${DB_ROOT_PASSWORD} -e "SHOW MASTER STATUS\G")
+MASTER_STATUS=$(mysql -h "${DB_HOST}" -uroot -p"${DB_ROOT_PASSWORD}" -e "SHOW MASTER STATUS\G")
 FILE=$(echo "$MASTER_STATUS" | grep File: | awk '{print $2}')
 POSITION=$(echo "$MASTER_STATUS" | grep Position: | awk '{print $2}')
 
 # Configurar la replicación
-mysql -uroot -p${DB_ROOT_PASSWORD} -e "
+mysql -uroot -p"${DB_ROOT_PASSWORD}" -e "
 CHANGE REPLICATION SOURCE TO
-SOURCE_HOST='gestcoop_db',
-SOURCE_USER='replicador',
-SOURCE_PASSWORD='replicapass',
+SOURCE_HOST='$DB_HOST',
+SOURCE_USER='$DB_REPLICATOR_USER',
+SOURCE_PASSWORD='$DB_REPLICATOR_PASSWORD',
 SOURCE_LOG_FILE='$FILE',
 SOURCE_LOG_POS=$POSITION;
 START REPLICA;
